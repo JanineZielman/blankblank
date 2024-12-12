1a44bfef49993ac1996ddcaba23de36f9112724b
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2024-12-12 17:00:17 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -2,44 +2,61 @@ import Head from "next/head";
 import { PrismicLink, PrismicText, PrismicRichText } from "@prismicio/react";
 import { PrismicNextImage } from "@prismicio/next";
 import * as prismicH from "@prismicio/helpers";
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 
 import { createClient } from "../prismicio";
 import { Layout } from "../components/Layout";
 
-
 const Index = ({ navigation, settings, home }) => {
   useEffect(() => {
-    $( function() {
-      const dragItems = document.getElementsByClassName('drag');
-      for (let i = 0; i < dragItems.length; i++) { 
-        var random1 = Math.floor(Math.random() * 20);
-        var random2 = Math.floor(Math.random() * 20);
-        $( `#drag${i}` ).draggable();
-        document.getElementById(`drag${i}`).style.marginLeft = random1 + 'vw'
-        document.getElementById(`drag${i}`).style.marginTop = random2 + 'vh'
+    const updatePositions = () => {
+      const dragItems = document.getElementsByClassName("drag");
+      for (let i = 0; i < dragItems.length; i++) {
+        const random1 = Math.floor(Math.random() * 20);
+        const random2 = Math.floor(Math.random() * 20);
+        const element = document.getElementById(`drag${i}`);
+        element.style.marginLeft = random1 + "vw";
+        element.style.marginTop = random2 + "vh";
       }
-    } );
-  }, [])
+    };
+
+    $(function () {
+      const dragItems = document.getElementsByClassName("drag");
+      for (let i = 0; i < dragItems.length; i++) {
+        $(`#drag${i}`).draggable();
+      }
+    });
+
+    // Initial position update
+    updatePositions();
+
+    // Update positions every 3 seconds
+    const interval = setInterval(updatePositions, 3000);
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <Layout
-      navigation={navigation}
-      settings={settings}
-    >
+    <Layout navigation={navigation} settings={settings}>
       <Head>
         <title>{prismicH.asText(settings.data.name)}</title>
       </Head>
       <div className="container">
         <div className="drag-container">
           {home.data.project.map((item, i) => {
-            return(
-              <div className={`drag ${item.position}`} id={`drag${i}`} key={`drag${i}`}>
+            return (
+              <div
+                className={`drag ${item.position}`}
+                id={`drag${i}`}
+                key={`drag${i}`}
+              >
                 <PrismicLink field={item.link}>
-                  <PrismicNextImage field={item.image}/>
-                  <PrismicRichText field={item.title}/>
+                  <PrismicNextImage field={item.image} />
+                  <PrismicRichText field={item.title} />
                 </PrismicLink>
               </div>
-            )
+            );
           })}
         </div>
       </div>
@@ -60,7 +77,7 @@ export async function getStaticProps({ previewData }) {
     props: {
       navigation,
       settings,
-      home
+      home,
     },
   };
 }
